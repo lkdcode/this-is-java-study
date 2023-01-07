f1377d6003cd438db846d0a09b0b1483b8f591bf
lkdcode <dlrlejr1@naver.com>
2023-01-08 00:16:17 +0900

    ✨ 유저가 매뉴를 선택할 때 입력받는 클래스
    initial commit

---
+++ src/main/java/UserInputOrder.java
@@ -0,0 +1,10 @@
+public class UserInputOrder implements UserInput {
+
+    /**
+     * @parm UserInput = 유저가 주문할 때 입력 받는 역할
+     */
+    @Override
+    public String userInput() {
+        return null;
+    }
+}
