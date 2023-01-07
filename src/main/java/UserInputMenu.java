5b5ed4cf7ddd5e327197609547a9910ec58875ac
lkdcode <dlrlejr1@naver.com>
2023-01-08 00:15:59 +0900

    ✨ 유저가 매뉴를 선택할 때 입력받는 클래스
    initial commit

---
+++ src/main/java/UserInputMenu.java
@@ -0,0 +1,9 @@
+public class UserInputMenu implements UserInput{
+    /**
+     * @parm UserInput = 유저가 매뉴를 선택할 때 입력받는 역할
+     */
+    @Override
+    public String userInput() {
+        return null;
+    }
+}
