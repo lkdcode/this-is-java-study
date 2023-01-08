5fa3c68568d5b8f337fe8a70fad47afe55199525
Alex Kim <alexkjh2011@gmail.com>
2023-01-08 10:27:39 +0900

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,17 @@
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+//import java.io.IOException;
+
+//public class Application {
+//    public static void main(String[] args) throws IOException {
+//
+//        System.out.println("Hello world! For our Project");
+//
+//        Cash cash = new Cash();
+//        cash.paypal();
+//>>>>>>> Stashed changes
     }
 }
