4168f9c142bd24c176c2cd59af18cc9b73ce703c
lkdcode <dlrlejr1@naver.com>
2023-01-08 12:58:29 +0900

    ✨ 유저로부터 메인 메뉴를 입력받는 클래스
    initial commit

---
+++ src/main/java/hope/user/menu/UserInputMainMenu.java
@@ -0,0 +1,16 @@
+package hope.user.menu;
+
+import hope.user.UserInput;
+
+/**
+ * 유저로부터 메인 메뉴를 입력받는 클래스
+ */
+public class UserInputMainMenu implements UserInput {
+
+
+    @Override
+    public String userInput() {
+        return null;
+    }
+
+}
