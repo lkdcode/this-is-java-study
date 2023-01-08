317b28eb46cc433dfbfbeac93862400fc0f2a2f3
lkdcode <dlrlejr1@naver.com>
2023-01-08 20:21:42 +0900

    ✨ 테이블 정보를 가지고 있는 클래스
    getTableBills() 추가

--- src/main/java/hope/data/Table.java
+++ src/main/java/hope/data/Table.java
@@ -1,14 +1,21 @@
+package hope.data;
+
 import java.util.ArrayList;
 import java.util.List;
 
-/** 테이블 정보를 가지고 있는 클래스
- *
+/**
+ * 테이블 정보를 가지고 있는 클래스
  */
-
 public class Table {
-
     private int number;
-    private List<TableBills> tableBills;
+    private List<TableOrder> tableBills;
+
+    public List<TableOrder> getTableBills() {
+        return tableBills;
+    }
+
+
+
     private String upperShape;
     private String middleShape;
     private String lowerShape;
@@ -30,8 +37,11 @@ public class Table {
         return price;
     }
 
+    /**
+     * 네이밍 수정
+     */
     public void setBill() {
-        TableBills tableOrder = new TableBills();
+        TableOrder tableOrder = new TableOrder();
         tableOrder.order("후라이드", 16000, 1);
         this.tableBills.add(tableOrder);
     }
