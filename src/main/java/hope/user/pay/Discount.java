fccfd0a670833c37b0eb26626c7a64f7df902415
lkdcode <dlrlejr1@naver.com>
2023-01-08 20:24:19 +0900

    🔥 결제 금액의 할인을 관리하는 클래스
    discount() 구현

--- src/main/java/hope/user/pay/Discount.java
+++ src/main/java/hope/user/pay/Discount.java
@@ -1,5 +1,7 @@
 package hope.user.pay;
 
+import hope.data.DataBase;
+
 /**
  * 결제금액의 할인을 관리하는 클래스
  */
@@ -7,8 +9,10 @@ public class Discount {
 
     /**
      * 결제금액의 할인을 적용하는 클래스
+     * 치킨 종류 상관없이
+     * 10개당 10,000원을 할인 적용하고
      */
-    public void discount() {
-
+    public int discount() {
+        return DataBase.getInstance().getBills();
     }
 }
