d1145e76a586b74f9eaa64b707447aa11eda03e7
lkdcode <dlrlejr1@naver.com>
2023-01-09 12:05:21 +0900

    🔥 유저로부터 메뉴 주문을 입력받은 값을 검증하는 클래스
    구현 완료

---
+++ src/main/java/hope/user/order/validate/UserInputOrderMenuNumberValidate.java
@@ -0,0 +1,56 @@
+package hope.user.order.validate;
+
+import hope.exception.ErrorMessage;
+
+public class UserInputOrderMenuNumberValidate extends UserInputTableNumberValidate {
+    private final String MENU = "^[1-6]*$";
+    private final int COKE = 21;
+    private final int SPRITE = 22;
+    private String userInput;
+
+    @Override
+    public boolean userInputValidate(String userInput) {
+        this.userInput = userInput;
+        super.userInput = this.userInput;
+
+        if (!isNumber()) {
+            ErrorMessage.ERROR_INPUT_NUMBER.print();
+            return false;
+        }
+
+        if (!isNumberRange()) {
+            ErrorMessage.ERROR_INPUT_MENU_ORDER.print();
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected boolean isNumber() {
+        return super.isNumber();
+    }
+
+    @Override
+    protected boolean isNumberRange() {
+        if (userInput.length() > 2) {
+            return false;
+        }
+
+        if (userInput.length() == 1) {
+            if (!(userInput.matches(MENU))) {
+                return false;
+            }
+        }
+
+        if (userInput.length() == 2) {
+            if (!(Integer.parseInt(this.userInput) == COKE
+                    || Integer.parseInt(this.userInput) == SPRITE)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
