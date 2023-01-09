707a5730daf7f54339602e795578fe9fd941adbc
lkdcode <dlrlejr1@naver.com>
2023-01-09 12:06:46 +0900

    🔥 유저가 주문을 할 때 테이블 넘버를 입력받는 클래스
    구현 완료

---
+++ src/main/java/hope/user/order/UserInputOrderTable.java
@@ -0,0 +1,37 @@
+package hope.user.order;
+
+import hope.UI.Message;
+import hope.data.DataBase;
+import hope.user.UserInput;
+import hope.user.order.validate.UserInputTableNumberValidate;
+
+import java.util.Scanner;
+
+/**
+ * 유저가 테이블 넘버를 입력받는 클래스
+ */
+public class UserInputOrderTable implements UserInput {
+    private final Scanner scanner;
+    private final UserInputTableNumberValidate userInputTableValidate;
+
+    public UserInputOrderTable() {
+        this.scanner = new Scanner(System.in);
+        this.userInputTableValidate = new UserInputTableNumberValidate();
+    }
+
+    @Override
+    public int userInput() {
+        boolean isTable;
+        String userInputTableNumber;
+        do {
+            Message.TABLE.print();
+            DataBase.getInstance().printTable();
+            Message.INPUT_TABLE.print();
+            userInputTableNumber = scanner.nextLine();
+            isTable = userInputTableValidate.userInputValidate(userInputTableNumber);
+        } while (!isTable);
+
+        return Integer.parseInt(userInputTableNumber);
+    }
+
+}
