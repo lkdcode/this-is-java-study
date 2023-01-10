ea08324e6624a253b53c54c25f245f35bb901941
lkdcode <dlrlejr1@naver.com>
2023-01-10 14:38:50 +0900

    🔥 카드 결제를 담당하는 클래스
    extends Discount 삭제
    리팩터링 필요

--- src/main/java/hope/user/pay/Card.java
+++ src/main/java/hope/user/pay/Card.java
@@ -1,38 +1,36 @@
 package hope.user.pay;
 
-import hope.data.Table;
+import hope.UI.Message;
 import hope.data.DataBase;
 
 import java.util.Scanner;
 
-public class Card extends Discount implements Pay {
+/**
+ * 카드 결제를 담당하는 클래스
+ */
+public class Card implements Pay {
+    private final Scanner scanner;
 
-    /**
-     * 카드 결제를 담당하는 클래스
-     */
+    public Card() {
+        scanner = new Scanner(System.in);
+    }
+
     @Override
-    public void paypal() {
-        int tableNumber = 1;
-        Table table = DataBase.getInstance().getTable(tableNumber);
+    public void paypal(int userTableNumber) {
+        int totalPrice = DataBase.getInstance().getBills(userTableNumber);
 
-        int totalPrice = discount();
-        Scanner scanner = new Scanner(System.in);
         int userPay;
-        System.out.println("총 결제 금액 : " + table.getPrice());
+        System.out.println("## 총 결제 금액 : " + totalPrice);
+        Message.PAYMENT_CARD.print();
 
         do {
-            System.out.print("결제하실 금액을 입력해주세요 : ");
-            userPay = scanner.nextInt();
-        } while (!(userPay == table.getPrice()));
+            System.out.print("## 결제하실 금액을 입력해주세요 : ");
 
-        System.out.println("결제 완료");
+            userPay = this.scanner.nextInt();
+        } while (!(userPay == totalPrice));
+
+        DataBase.getInstance().paid(userTableNumber);
 
     }
-
-    @Override
-    public int discount() {
-        return super.discount();
-    }
-
 
 }
