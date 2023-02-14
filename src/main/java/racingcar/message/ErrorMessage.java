590dee2e1ac6975c9206cede2e2b3bb66cb20090
lkdcode <dlrlejr1@naver.com>
2023-02-14 17:18:03 +0900

    🔥에러 메시지 수정
    println() -> printf() 로 변경

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -4,8 +4,8 @@ package racingcar.message;
  * Exception 발생 시 출력할 메시지를 가지고 있는 클래스
  */
 public enum ErrorMessage {
-    ERROR_INPUT_CAR_NAME("[ERROR] 최소 1명이상 입력하여야 합니다. (쉼표(,) 기준으로 구분)"),
-    ERROR_INPUT_LAT_TIME("[ERROR] 시도 횟수는 숫자여야 합니다.");
+    ERROR_INPUT_CAR_NAME("[ERROR] 최소 1명이상 입력하여야 합니다. (쉼표(,) 기준으로 구분)%n%n"),
+    ERROR_INPUT_LAT_TIME("[ERROR] 시도 횟수는 숫자여야 하며 최소 1 이상이어야 합니다.%n%n");
     private String view;
 
     ErrorMessage(String view) {
@@ -13,6 +13,6 @@ public enum ErrorMessage {
     }
 
     public void print() {
-        System.out.println(this.view);
+        System.out.printf(this.view);
     }
 }
