e9542d0d66da62c68ca76a8bb8015421b4f60d87
lkdcode <dlrlejr1@naver.com>
2023-02-14 17:17:30 +0900

    🔥자동차 클래스 일급 콜렉션 구현
    경기에 임할 자동차 한 데 모아 일급 콜렉션으로 구현하였다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,11 +3,26 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 자동차를 담고 있는 일급 콜렉션
+ */
 public class Cars {
     private List<Car> cars;
 
     public Cars() {
         this.cars = new ArrayList<>();
+    }
+
+    public void makeCars(String[] carNames) {
+        for (String name : carNames) {
+            this.cars.add(new Car(name));
+        }
+    }
+
+    public void move() {
+        for (Car car : this.cars) {
+            car.move();
+        }
     }
 
 }
