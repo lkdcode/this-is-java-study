4de720b0deb29a5b13b451fd390cb2bf1db94070
lkdcode <dlrlejr1@naver.com>
2023-02-16 14:45:29 +0900

    🔥레이싱 결과를 출력하는 클래스
    1차 구현 완료
    리팩터링 필요

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,11 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 유저에게 게임 진행 결과를 출력하는 클래스
  */
@@ -8,4 +14,46 @@ public class OutputView {
     public OutputView() {
     }
 
+    public void playResultPrint(List<Car> cars) {
+
+        for (int i = 0; i < cars.size(); i++) {
+            Car player = cars.get(i);
+            StringBuilder resultPrint = new StringBuilder();
+            resultPrint.append(player.getName() + " : ");
+            for (int j = 0; j < player.getPosition(); j++) {
+                resultPrint.append("-");
+            }
+            System.out.println(resultPrint);
+        }
+        System.out.println();
+    }
+
+    public void finalWinner(List<Car> cars) {
+
+        int move = Integer.MIN_VALUE;
+
+        for (int i = 0; i < cars.size(); i++) {
+            Car player = cars.get(i);
+            move = Math.max(player.getPosition(), move);
+        }
+
+        List<String> winnerNames = new ArrayList<>();
+
+        for (int i = 0; i < cars.size(); i++) {
+            Car player = cars.get(i);
+            if (player.getPosition() == move) {
+                winnerNames.add(player.getName());
+            }
+        }
+
+        Collections.sort(winnerNames);
+
+        System.out.print("최종 우승자는 : ");
+        for (int i = 0; i < winnerNames.size(); i++) {
+            System.out.print(winnerNames.get(i));
+            if (i != winnerNames.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
 }
