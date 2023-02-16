9907091223dac89ebcd38addee6f2b2cfbb6f8a5
lkdcode <dlrlejr1@naver.com>
2023-02-16 14:44:42 +0900

    🔥자동차 객체 구현 완료

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,7 +5,7 @@ package racingcar.domain;
  */
 public class Car {
     private final String name;
-    private Integer position;
+    private Integer position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -13,7 +13,7 @@ public class Car {
 
     // TODO : 추가 기능 구현
     public void move() {
-        this.position = new RandomMove().get();
+        this.position += new RandomMove().get();
     }
 
     public String getName() {
