8c0c729aef0d3d4da05d5315e7322b30fb7b8cca
lkdcode <dlrlejr1@naver.com>
2023-03-06 22:56:49 +0900

    출력하는 책임 제거, 단순히 문자열만 리턴하도록 수정

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -2,9 +2,11 @@ package racingcar.message;
 
 /**
  * Exception 발생 시 출력할 메시지를 가지고 있는 클래스
+ * TODO :
+ * 출력하는 책임 제거. 단순히 문자열만 리턴
  */
 public enum ErrorMessage {
-    ERROR_INPUT_CAR_NAME("[ERROR] 최소 1명이상 입력하여야 합니다. (쉼표(,) 기준으로 구분)%n%n"),
+    ERROR_INPUT_CAR_NAME("[ERROR] 최소 2명이상 입력하여야 합니다. (쉼표(,) 기준으로 구분)%n%n"),
     ERROR_INPUT_LAT_TIME("[ERROR] 시도 횟수는 숫자여야 하며 최소 1 이상이어야 합니다.%n%n");
     private String view;
 
@@ -12,7 +14,7 @@ public enum ErrorMessage {
         this.view = view;
     }
 
-    public void print() {
-        System.out.printf(this.view);
+    public String message() {
+        return view;
     }
 }
