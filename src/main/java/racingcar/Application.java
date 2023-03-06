8a514af1a1ee2c80a4517aa6eade3933cd2e14f5
lkdcode <dlrlejr1@naver.com>
2023-03-06 22:55:01 +0900

    Application
    SystemController 메소드명 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,6 @@ public class Application {
     private static final SystemController systemController = new SystemController();
 
     public static void main(String[] args) {
-        systemController.start();
+        systemController.playGame();
     }
 }
