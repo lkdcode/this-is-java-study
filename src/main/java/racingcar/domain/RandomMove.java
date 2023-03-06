6b073bb7151c48559fefca79b04ddbc7a8023403
lkdcode <dlrlejr1@naver.com>
2023-03-06 22:57:53 +0900

    래핑 추가, 싱글톤으로 구현

--- src/main/java/racingcar/domain/RandomMove.java
+++ src/main/java/racingcar/domain/RandomMove.java
@@ -2,13 +2,26 @@ package racingcar.domain;
 
 /**
  * 랜덤으로 번호를 생성하여 전진 조건에 맞는지 판단 후 리턴하는 클래스
+ * TODO :
+ * 랩핑, 싱글톤
  */
 public class RandomMove {
-    public Integer get() {
-        if ((int) (Math.random() * 10) >= 4) {
-            return 1;
+    private static RandomMove instance;
+    private int runnable = 4;
+    private int stop = 0;
+    private int go = 1;
+
+    public static RandomMove getInstance() {
+        if (instance == null) {
+            instance = new RandomMove();
         }
-        return 0;
+        return instance;
     }
 
+    public int move() {
+        if ((int) (Math.random() * 10) >= runnable) {
+            return go;
+        }
+        return stop;
+    }
 }
